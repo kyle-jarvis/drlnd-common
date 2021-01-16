01e8effd85fdfd15d2cacde147e765b731b0d9c6
Kyle Jarvis <kyle.jarvis1905@gmail.com>
2021-01-16 03:44:09 +0000

    Setup script and package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='drlnd-common',
+      version='0.1',
+      description='Commons for drlnd repos',
+      author='Kyle Jarvis',
+      packages=['drlnd'],
+     )
